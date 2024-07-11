5131d668a3d7611918f270df5663661e96141a88
Selerek <90503520+Selerek@users.noreply.github.com>
2024-07-11 10:19:42 +0200

    mapa created mocnooo
    nadchodzi pieeklo no bo kazda dziewczynka chcialaby zostac influencerką

--- HaveASeat/src/app/mapa/mapa.component.ts
+++ HaveASeat/src/app/mapa/mapa.component.ts
@@ -1,31 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-mapa',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './mapa.component.html',
-  styleUrl: './mapa.component.css'
+  styleUrls: ['./mapa.component.css']
 })
-export class MapaComponent {
+export class MapaComponent implements OnInit {
+  roomWidth = 15; 
+  roomHeight = 11; 
+  
 
-  //to są testy na razie prosze nie bic pls <3333
-  map = [
-    [
-      { status: 'free' },
-      { status: 'reserved' },
-      { status: 'yours' }
-    ],
-    [
-      { status: 'yours' },
-      { status: 'free' },
-      { status: 'reserved' }
-    ],
-    [
-      { status: 'reserved' },
-      { status: 'yours' },
-      { status: 'free' }
-    ]
-  ];
+  ngOnInit(): void {}
 }
